fab4259b90447580d7ee825ccb78e78f9cf7d5af
cad <icaddicad5@gmail.com>
2023-12-04 14:36:03 +0700

    UI Admin site

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-   
+    return view('admin');
 });
 
+Route::get('/edit.blade.php', function () {
+    return view('edit');
+});
+
+Route::get('/tambah_data.blade.php', function () {
+    return view('tambah_data');
+});
+
+Route::get('/detail_talent.blade.php', function () {
+    return view('detail_talent');
+});
+
+Route::get('/admin.blade.php', function () {
+    return view('admin');
+});
+
+Route::get('/statistik.blade.php', function () {
+    return view('statistik');
+});
+
+Route::get('/statistik_detail.blade.php', function () {
+    return view('statistik_detail');
+});
