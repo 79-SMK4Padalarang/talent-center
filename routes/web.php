6f7a0af27342520cd0fb73ce1879cbb1f16b394f
sasya <sasya.syahidahnr@gmail.com>
2023-12-08 10:26:48 +0700

    merge search autocomplete

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('admin');
+});
+Route::get('/', function () {
+    return view('welcome');
 });
 Route::get('/client-detail', function () {
     return view('client-detail');
